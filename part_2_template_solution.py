0f91e76f89713a1d9df188cd8a4a6a603f002097
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-02-26 21:08:11 -0500

    Update part_2_template_solution.py

--- part_2_template_solution.py
+++ part_2_template_solution.py
@@ -71,8 +71,25 @@ class Section2:
 
         Xtrain = Xtest = np.zeros([1, 1], dtype="float")
         ytrain = ytest = np.zeros([1], dtype="int")
+        Xtest = nu.scale_data(Xtest)
+        
+        answer = {}
+        answer["nb_classes_train"] = len(np.unique(ytrain))
+        answer["nb_classes_test"] = len(np.unique(ytest))
+        answer["class_count_train"] = np.bincount(ytrain)
+        answer["class_count_test"] = np.bincount(ytest)
+        answer["length_Xtrain"] = len(Xtrain)
+        answer["length_Xtest"] = len(Xtest)
+        answer["length_ytrain"] = len(ytrain)
+        answer["length_ytest"] = len(ytest)
+        answer["max_Xtrain"] = Xtrain.max()
+        answer["max_Xtest"] = Xtest.max()
+        
+        #Xtrain = Xtest = np.zeros([1, 1], dtype="float")
+        #ytrain = ytest = np.zeros([1], dtype="int")
 
         return answer, Xtrain, ytrain, Xtest, ytest
+
 
     """
     B.  Repeat part 1.C, 1.D, and 1.F, for the multiclass problem. 
@@ -102,6 +119,78 @@ class Section2:
         """ """
         # Enter your code and fill the `answer`` dictionary
         answer = {}
+        train_list = ntrain_list
+        test_list = ntest_list
+        for i in range(0,len(train_list)):
+            train_val = train_list[i]
+            test_val= test_list[i]
+            Xtrain = X[0:train_val, :]
+            ytrain = y[0:train_val]
+            Xtest = Xtest[0:test_val, :]
+            ytest = ytest[0:test_val]
+            
+            partC = {}
+            dt_clf=DecisionTreeClassifier(random_state=self.seed)
+            K_cv=KFold(n_splits=5,random_state=self.seed,shuffle=True)
+            partC_results=u.train_simple_classifier_with_cv(Xtrain=Xtrain,ytrain=ytrain,
+                                              clf=dt_clf,
+                                              cv=K_cv)
+            partC_scores={}
+            partC_scores['mean_fit_time']=partC_results['fit_time'].mean()
+            partC_scores['std_fit_time']=partC_results['fit_time'].std()
+            partC_scores['mean_accuracy']=partC_results['test_score'].mean()
+            partC_scores['std_accuracy']=partC_results['test_score'].std()
+                    
+            partC["scores"] = partC_scores
+            partC["clf"] = dt_clf 
+            partC["cv"] = K_cv  
+
+            partD = {}
+            Sh_cv=ShuffleSplit(n_splits=5,random_state=self.seed)
+            partD_results=u.train_simple_classifier_with_cv(Xtrain=Xtrain,ytrain=ytrain,
+                                              clf=dt_clf,
+                                              cv=Sh_cv)
+
+            partD_scores={}
+            partD_scores['mean_fit_time']=partD_results['fit_time'].mean()
+            partD_scores['std_fit_time']=partD_results['fit_time'].std()
+            partD_scores['mean_accuracy']=partD_results['test_score'].mean()
+            partD_scores['std_accuracy']=partD_results['test_score'].std()
+            
+            partD["scores"] = partD_scores
+    
+            partD["clf"] = dt_clf
+            partD["cv"] = Sh_cv
+
+            partF={}
+            
+            clf_LR=LogisticRegression(random_state=self.seed,max_iter=300)
+            
+            partF_results=u.train_simple_classifier_with_cv(Xtrain=Xtrain,ytrain=ytrain,
+                                              clf=clf_LR,
+                                              cv=Sh_cv)
+            clf_LR.fit(Xtrain, ytrain)
+
+            partF['scores_train_F'] = accuracy_score(ytrain, clf_LR.predict(Xtrain))
+            partF['scores_test_F'] = accuracy_score(ytest, clf_LR.predict(Xtest))
+                    
+            partF['mean_cv_accuracy_F']=partF_results['test_score'].mean()
+
+            partF["clf_LR"] = clf_LR
+            partF["cv"] = Sh_cv
+
+            partF['conf_mat_train'] = confusion_matrix(ytrain, clf_LR.predict(Xtrain))
+            partF['conf_mat_test'] = confusion_matrix(ytest, clf_LR.predict(Xtest))
+
+        
+            answer[train_val] = {}
+            answer[train_val]["partC"] = partC
+            answer[train_val]["partD"] = partD
+            answer[train_val]["partF"] = partF
+            answer[train_val]["ntrain"] = train_val
+            answer[train_val]["ntest"] = test_val
+            answer[train_val]["class_count_train"] = list(np.bincount(ytrain))
+            answer[train_val]["class_count_test"] = list(np.bincount(ytest))
 
         """
         `answer` is a dictionary with the following keys:
